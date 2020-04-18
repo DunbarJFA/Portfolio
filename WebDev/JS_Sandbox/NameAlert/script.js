10e8b7f8b8f17e7f7e14abb2aefab6858f1ecc39
Jerod <dunbar.jfa@gmail.com>
2020-04-18 13:45:04 -0500

    Completed NameAlert Javascript practice

---
+++ WebDev/JS_Sandbox/NameAlert/script.js
@@ -0,0 +1,12 @@
+var button = document.getElementById("btn");
+var input = document.getElementById('input');
+
+button.onclick = function(){
+    if (input.value.trim() == ""){
+        alert("Please tell us what to call you!");
+    }
+    else{
+        alert(`Hello, ${input.value.trim()}!`);
+        console.log(input.value.trim());
+    }
+};
